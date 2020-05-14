07a8b56f6c9e6cb6b8cc131ae477941b435e57f0
Benjamin Wright <bwright@trilogyed.com>
2020-05-14 12:55:22 -0400

    Wrote test to test if server is running

--- test/server.test.js
+++ test/server.test.js
@@ -1,9 +1,14 @@
-const axios = require(axios);
+const axios = require("axios");
 
 describe("Server", () => {
-  it("should deliver a staus that is not 500 ", () => {
-    const response = axios.get("/");
-
-    expect(response.status).notToBe(500);
+  it("should deliver a staus that is not 500 ", async () => {
+    try {
+      const { status } = await axios.get("/ben");
+      //console.log(status);
+      expect(status).not.toBe(undefined);
+    } catch (err) {
+      const { status } = err.response;
+      expect(status).not.toBe(undefined);
+    }
   });
-})
+});
